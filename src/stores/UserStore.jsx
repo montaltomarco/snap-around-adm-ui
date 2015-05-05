718e8714bc112b7e3c353c72062b402eb7efab44
Robin Ricard <ricard.robin@gmail.com>
2015-05-05 20:52:32 +0200

    Add new actions to the User

--- src/stores/UserStore.jsx
+++ src/stores/UserStore.jsx
@@ -2,7 +2,11 @@
 
 import {Atom} from "atomstore";
 
-import {FETCH_USERS} from "../actions/UserActions.jsx";
+import {
+  FETCH_USERS,
+  CHECK_USER,
+  UPDATE_USER_PATH
+} from "../actions/UserActions.jsx";
 import Dispatcher from "../dispatcher.jsx";
 
 let UserStore = new Atom([]);
@@ -11,10 +15,39 @@ function replaceWith(list) {
   return list;
 }
 
+function matchAttrEquality(key, value) {
+  return (user) => {
+    return (user[key] === value);
+  };
+}
+
+function assocMatching(matchingFn, key, value) {
+  return this.map((user) => {
+    if(matchingFn(user)) {
+      return user.set(key, value);
+    }
+    return user;
+  });
+}
+
 UserStore.dispatchToken = Dispatcher.register((payload) => {
   switch(payload.get("actionType")) {
     case FETCH_USERS:
       UserStore.swap(replaceWith, payload.get("users"));
+      break;
+    case CHECK_USER:
+      UserStore.swap(
+        assocMatching,
+        matchAttrEquality("fbUserId", payload.fbUserId),
+        "checked",
+        payload.checked);
+      break;
+    case UPDATE_USER_PATH:
+      UserStore.swap(
+        assocMatching,
+        matchAttrEquality("fbUserId", payload.fbUserId),
+        "path",
+        payload.path);
       break;
   }
 });
